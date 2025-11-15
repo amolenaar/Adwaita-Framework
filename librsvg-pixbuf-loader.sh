c157240e006f86da366d636b89aca130c7a012dd
Arjan Molenaar <gaphor@gmail.com>
2025-11-15 17:07:22 +0100

    Disable introspection
    ... as it doesn't play well with subprojects.

--- librsvg-pixbuf-loader.sh
+++ librsvg-pixbuf-loader.sh
@@ -3,6 +3,6 @@
 # It will recompile librsvg with pixbuf loader enabled.
 
 meson setup --prefix=$1 --pkg-config-path=$1/lib/pkgconfig --wipe \
-    -Dpixbuf=enabled -Davif=disabled -Dpixbuf-loader=enabled -Ddocs=disabled -Dtests=false
+    -Dpixbuf=enabled -Davif=disabled -Dpixbuf-loader=enabled -Ddocs=disabled -Dtests=false \
     _build_rsvg subprojects/librsvg
 meson install -C _build_rsvg
