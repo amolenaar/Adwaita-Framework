edf8b7547799265d7628569e79388a02be90a93e
Arjan Molenaar <gaphor@gmail.com>
2026-06-10 21:53:26 +0200

    Fix rpaths now that all libraries are bundled with gdk-pixbuf

--- scripts/fix-gdk-pixbuf-rpath.sh
+++ scripts/fix-gdk-pixbuf-rpath.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-install_name_tool -change @rpath/libpng16.16.dylib \
-    "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/libpng16.16.dylib" \
-    "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/libgdk_pixbuf-2.0.0.dylib"
-install_name_tool -change @rpath/libjpeg.8.2.2.dylib \
-    "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/libjpeg.8.2.2.dylib" \
-    "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/libgdk_pixbuf-2.0.0.dylib"
+INSTALLDIR="${DESTDIR}/${MESON_INSTALL_PREFIX}"
+# replace double slash in path with a single one
+INSTALLDIR="${INSTALLDIR//\/\///}"
 
-"${DESTDIR}/${MESON_INSTALL_PREFIX}/bin/gdk-pixbuf-query-loaders" > "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
+install_name_tool -change @rpath/libpng16.16.dylib \
+    "${INSTALLDIR}/lib/libpng16.16.dylib" \
+    "${INSTALLDIR}/lib/libgdk_pixbuf-2.0.0.dylib"
+install_name_tool -change @rpath/libjpeg.8.2.2.dylib \
+    "${INSTALLDIR}/lib/libjpeg.8.2.2.dylib" \
+    "${INSTALLDIR}/lib/libgdk_pixbuf-2.0.0.dylib"
+install_name_tool -change @rpath/libtiff.6.dylib \
+    "${INSTALLDIR}/lib/libtiff.6.dylib" \
+    "${INSTALLDIR}/lib/libgdk_pixbuf-2.0.0.dylib"
