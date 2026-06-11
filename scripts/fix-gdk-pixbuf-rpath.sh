6fa79bec3a8cd5cea9f6da9f4cfe402092a08763
Arjan Molenaar <gaphor@gmail.com>
2026-06-11 11:31:57 +0200

    Update wrapdb wraps

--- scripts/fix-gdk-pixbuf-rpath.sh
+++ scripts/fix-gdk-pixbuf-rpath.sh
@@ -4,12 +4,11 @@ INSTALLDIR="${DESTDIR}/${MESON_INSTALL_PREFIX}"
 # replace double slash in path with a single one
 INSTALLDIR="${INSTALLDIR//\/\///}"
 
-install_name_tool -change @rpath/libpng16.16.dylib \
-    "${INSTALLDIR}/lib/libpng16.16.dylib" \
-    "${INSTALLDIR}/lib/libgdk_pixbuf-2.0.0.dylib"
-install_name_tool -change @rpath/libjpeg.8.2.2.dylib \
-    "${INSTALLDIR}/lib/libjpeg.8.2.2.dylib" \
-    "${INSTALLDIR}/lib/libgdk_pixbuf-2.0.0.dylib"
-install_name_tool -change @rpath/libtiff.6.dylib \
-    "${INSTALLDIR}/lib/libtiff.6.dylib" \
-    "${INSTALLDIR}/lib/libgdk_pixbuf-2.0.0.dylib"
+otool -L "${INSTALLDIR}/lib/libgdk_pixbuf-2.0.0.dylib" | \
+grep @rpath | cut -f 1 -d ' ' | xargs -L 1 basename | \
+while read LIB
+do
+    install_name_tool -change @rpath/$LIB \
+        "${INSTALLDIR}/lib/$LIB" \
+        "${INSTALLDIR}/lib/libgdk_pixbuf-2.0.0.dylib"
+done
