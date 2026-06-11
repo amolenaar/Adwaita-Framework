ff4b3af9e51132d425bb16f4cff665200441c86f
Arjan Molenaar <gaphor@gmail.com>
2026-06-11 19:46:48 +0200

    Libraries should use the install prefix, not real path
    This handles cases where everything is installed with `--destdir`.

--- scripts/fix-gdk-pixbuf-rpath.sh
+++ scripts/fix-gdk-pixbuf-rpath.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 INSTALLDIR="${DESTDIR}/${MESON_INSTALL_PREFIX}"
-# replace double slash in path with a single one
-INSTALLDIR="${INSTALLDIR//\/\///}"
 
 otool -L "${INSTALLDIR}/lib/libgdk_pixbuf-2.0.0.dylib" | \
 grep @rpath | cut -f 1 -d ' ' | xargs -L 1 basename | \
 while read LIB
 do
     install_name_tool -change @rpath/$LIB \
-        "${INSTALLDIR}/lib/$LIB" \
+        "${MESON_INSTALL_PREFIX}/lib/$LIB" \
         "${INSTALLDIR}/lib/libgdk_pixbuf-2.0.0.dylib"
 done
